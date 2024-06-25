1769d0c9bc290d4faae1bdbc63833b4aad8271eb
aaa <aaa@syn-172-100-007-063.res.spectrum.com>
2024-06-25 14:38:10 +0900

    Loading 페이지 메인 넘어가도록

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoadingPage() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/main");
+    }, 500);
+  }, []);
   return <div>로딩</div>;
 }
